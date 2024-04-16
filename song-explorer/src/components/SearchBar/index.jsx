0b3aac6dce3eb92e5bbff9687400d1b305dc5ad0
3hui112 <shinetheworld112@gmail.com>
2024-04-16 20:50:33 +0900

    third commit

--- song-explorer/src/components/SearchBar/index.jsx
+++ song-explorer/src/components/SearchBar/index.jsx
@@ -6,14 +6,19 @@ import { getSongs } from '../../apis/song';
 import { songsAtom } from '../../store/songs';
 import axios from 'axios'
 
-function SearchBar() {
+function SearchBar(props) {
   const setData = useSetRecoilState(songsAtom);
   const inputRef = useRef();
 
   const handleSearch = () => {
     console.log('search');
     
-    axios.get('https://api.manana.kr/karaoke/song/${inputRef.current.value}.json');
+    axios
+    .get('https://api.manana.kr/karaoke/song/${inputRef.current.value}.json')
+    .then((res) => {
+        console.log(res.data);
+        props.setSongList(res.data);
+    });
     getSongs(inputRef.current.value).then((res) => {
       setData(res.data);
     });
